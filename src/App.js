8f9f5a14c4608c4dc5d10c9d04cb262b9443b927
anastafish <anastafish24@gmail.com>
2023-02-14 20:39:04 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="grid grid-cols-3 grid-rows-[repeat(10,1fr)] sm:px-20 px-7 sm:py-11 py-7 h-[100vh]">
       <Navbar />
       <Main />
-      <Cards/>
+      <Cards />
     </div>
   );
 }
